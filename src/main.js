27fe64d6fc34c291edf67567a8b5a0142cb3d1cd
Moer <199525807@qq.com>
2017-07-17 09:38:42 +0800

    加入配置

--- src/main.js
+++ src/main.js
@@ -2,7 +2,28 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+
+// 路由集合
+import router from './config/router';
+
+// http 模块
+import axios from './config/axios';
+Vue.use(axios,{
+    // baseURL: "http://api.cjb789.com",
+    // baseURL: "http://test.cjb789.com",
+    baseURL: "http://back.7178861.com",
+    timeout: 10000,
+    withCredentials: true,
+});
+
+
+// 公共事件监听器
+import eventHub from './config/eventHub';
+Vue.use(eventHub);
+
+// 公共数据
+import globalMethods from './config/globalMethods';
+Vue.use(globalMethods);
 
 Vue.config.productionTip = false
 
