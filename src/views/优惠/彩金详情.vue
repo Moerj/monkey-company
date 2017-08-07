0b34f2962f9cda39e66a14e26bd8961e325337d5
Moer <199525807@qq.com>
2017-08-07 11:32:21 +0800

    删除详情的编辑功能

--- src/views/优惠/彩金详情.vue
+++ src/views/优惠/彩金详情.vue
@@ -7,80 +7,45 @@
     <ui-main title="彩金详情">
         <el-form v-if="data" label-width="120px" style="width:500px">
             <el-form-item label="彩金类型">
-                <el-select v-if="isEdit" v-model="form.type">
-                    <el-option :value="1" label="白菜">白菜</el-option>
-                    <el-option :value="2" label="固定存送">固定存送</el-option>
-                    <el-option :value="3" label="比例存送">比例存送</el-option>
-                </el-select>
-                <span v-else>
-                    <i v-if="data.type==1">白菜</i>
-                    <i v-if="data.type==2">固定存送</i>
-                    <i v-if="data.type==3">比例存送</i>
-                </span>
+                <i v-if="data.type==1">白菜</i>
+                <i v-if="data.type==2">固定存送</i>
+                <i v-if="data.type==3">比例存送</i>
             </el-form-item>
             <el-form-item :label="amountName">
-                <el-input v-if="isEdit" v-model="form.amount"></el-input>
-                <span v-else>{{data.amount}}</span>
+                <span>{{data.amount}}</span>
             </el-form-item>
             <el-form-item label="赠送金额">
-                <el-input v-if="isEdit"></el-input>
-                <span v-else>无字段</span>
+                <span>无字段</span>
             </el-form-item>
             <el-form-item label="存送比例">
-                <el-input v-if="isEdit" v-model="form.scale"></el-input>
-                <span v-else>{{data.scale}}%</span>
+                <span>{{data.scale}}%</span>
             </el-form-item>
             <el-form-item label="最高赠送">
-                <el-input v-if="isEdit" v-model="form.max"></el-input>
-                <span v-else>{{data.max}}</span>
+                <span>{{data.max}}</span>
             </el-form-item>
             <el-form-item label="提款上限">
-                <el-input v-if="isEdit" v-model="form.r_limit"></el-input>
-                <span v-else>{{data.r_limit}}</span>
+                <span>{{data.r_limit}}</span>
             </el-form-item>
             <el-form-item label="流水要求">
-                <el-input v-if="isEdit" v-model="form.multiple"></el-input>
-                <span v-else>{{data.multiple}}</span>
+                <span>{{data.multiple}}</span>
             </el-form-item>
             <el-form-item label="会员日限量">
-                <el-input v-if="isEdit" v-model="form.user_day_num"></el-input>
-                <span v-else>{{data.user_day_num}}</span>
+                <span>{{data.user_day_num}}</span>
             </el-form-item>
             <el-form-item label="每日总限量">
-                <el-input v-if="isEdit" v-model="form.day_num"></el-input>
-                <span v-else>{{data.day_num}}</span>
+                <span>{{data.day_num}}</span>
             </el-form-item>
             <el-form-item label="领取要求">
-                <el-select v-if="isEdit" v-model="form.coupon_request_arry" multiple style="width:500px">
-                    <el-option v-for="item in couponRequestOpt" :key="item.id" :value="item.id" :label="item.name"></el-option>
-                </el-select>
-                <div v-else>
-                    <el-tag v-for="item in data.couponRequest" :key="item.id" type="primary" class="mr5">{{item.name}}</el-tag>
-                </div>
+                <el-tag v-for="item in data.couponRequest" :key="item.id" type="primary" class="mr5">{{item.name}}</el-tag>
             </el-form-item>
             <el-form-item label="领取方式">
-                <el-select v-if="isEdit" v-model="form.draw_type">
-                    <el-option v-for="item in drawTypeOpt" :key="item.id" :value="item.id" :label="item.name"></el-option>
-                </el-select>
-                <div v-else>
-                    <el-tag type="primary" class="mr5">{{data.drawType.name}}</el-tag>
-                </div>
+                <el-tag type="primary" class="mr5">{{data.drawType.name}}</el-tag>
             </el-form-item>
             <el-form-item label="详细列表">
                 <el-button type="text" @click="$router.push({path:'/youhui',query:{tab:'2'}})">查看</el-button>
             </el-form-item>
             <el-form-item label="活动规则">
-                <el-input type="textarea" v-if="isEdit"></el-input>
-                <div v-else v-html="data.desc"></div>
-            </el-form-item>
-            <el-form-item>
-                <div class="ml50">
-                    <div v-if="isEdit">
-                        <el-button @click="isEdit=false">取消</el-button>
-                        <el-button type="primary">保存</el-button>
-                    </div>
-                    <el-button v-else type="primary" @click="isEdit=true">编辑</el-button>
-                </div>
+                <div v-html="data.desc"></div>
             </el-form-item>
         </el-form>
     </ui-main>
@@ -91,19 +56,6 @@
             return {
                 isEdit:false,
                 data:null,//查看的数据
-                form:{
-                    type:1,
-                    amount:'',
-                    scale:'',
-                    max:'',
-                    r_limit:'',
-                    multiple:'',
-                    user_day_num:'',
-                    day_num:'',
-                    coupon_request_arry:[],
-                    coupon_request:'',
-                    draw_type:''
-                },//编辑的数据
                 couponRequestOpt:[],
                 drawTypeOpt:[]
             }
@@ -159,15 +111,5 @@
                 return '金额'
             }
         },
-        watch: {
-            // 转换coupon_request为字符串 | 分割
-            'form.coupon_request_arry'(v){
-                this.form.coupon_request = ''
-                v.forEach((item)=>{
-                    this.form.coupon_request += item +'|'
-                })
-                this.form.coupon_request = this.form.coupon_request.replace(/\|$/,'')
-            }
-        }
     }
 </script>
